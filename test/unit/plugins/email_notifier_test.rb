487b1a7597ca494d4715dd65211077f991778518
Jon Ramsey <jonathon.ramsey@gmail.com>
2011-08-05 15:24:34 +0100

    Fix the tests I broke.

--- test/unit/plugins/email_notifier_test.rb
+++ test/unit/plugins/email_notifier_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class EmailNotifierTest < ActiveSupport::TestCase
   include FileSandbox
-  
+
   BUILD_LOG = <<-EOL
     blah blah blah
     something built
@@ -22,18 +22,18 @@ class EmailNotifierTest < ActiveSupport::TestCase
     @notifier = EmailNotifier.new
     @notifier.emails = ["default@gmail.com", "ccrb@thoughtworks.com"]
     @notifier.from = 'cruisecontrol@thoughtworks.com'
-    
+
     @project.add_plugin(@notifier, :test_email_notifier)
   end
-  
+
   teardown do
     teardown_sandbox
   end
 
   context "#build_finished" do
-    test "should not send an email for a passing build" do
+    test "should send an email for a passing build" do
       @notifier.build_finished(@build)
-      assert_equal [], ActionMailer::Base.deliveries
+      assert_equal 1, ActionMailer::Base.deliveries.size
     end
 
     test "should send an email if the build fails" do
@@ -45,23 +45,11 @@ class EmailNotifierTest < ActiveSupport::TestCase
       assert_equal "[CruiseControl] myproj build 5 failed", mail.subject
     end
 
-    test "should send an email for a passing build that fixes a failing build" do
-      Configuration.stubs(:dashboard_url).returns(nil)
-      @build.expects(:output).at_least_once.returns(BUILD_LOG)
-
-      @notifier.build_fixed(@build, @previous_build)
-
-      mail = ActionMailer::Base.deliveries[0]
-
-      assert_equal @notifier.emails, mail.to
-      assert_equal "[CruiseControl] myproj build 5 fixed", mail.subject
-    end
-
     test "should not log any sent emails if none are sent" do
       CruiseControl::Log.expects(:event).never
       BuildMailer.expects(:build_report).never
       @notifier.emails = []
-      @notifier.build_finished(failing_build)      
+      @notifier.build_finished(failing_build)
     end
 
     test "should log a single recipient if an email is sent" do
@@ -83,11 +71,11 @@ class EmailNotifierTest < ActiveSupport::TestCase
       CruiseControl::Log.expects(:event).with("Error sending e-mail - current server settings are :\n  :foo = 5", :error)
       mock_mail = mock("Email")
       mock_mail.expects(:deliver).raises('oh noes!')
-      
+
       BuildMailer.expects(:build_report).returns mock_mail
-      
+
       @notifier.emails = ['foo@crapty.com']
-      
+
       assert_raise_with_message(RuntimeError, 'oh noes!') do
         @notifier.build_finished(failing_build)
       end
@@ -97,20 +85,29 @@ class EmailNotifierTest < ActiveSupport::TestCase
       Configuration.expects(:email_from).returns('central@foo.com')
       @notifier.from = nil
       build = failing_build()
-      
+
       BuildMailer.expects(:build_report).with(build, ['default@gmail.com', 'ccrb@thoughtworks.com'],
-                          'central@foo.com', 'myproj build 5 failed', 'The build failed.').returns mock(:deliver => true)
+                          'central@foo.com', 'myproj build 5 failed', 'Build status: failed.').returns mock(:deliver => true)
 
       @notifier.build_finished(failing_build)
     end
 
-    test "should provide the URL of the build in the notification email" do
+    test "should provide the URL of the build in the notification email if dashboard url is set" do
       Configuration.stubs(:dashboard_url).returns("http://www.my.com")
       @notifier.emails = ['foo@happy.com']
       @notifier.build_finished(failing_build)
-      
+
       mail = ActionMailer::Base.deliveries[0]
       assert_match /http:\/\/www.my.com\/builds\/myproj\/5/, mail.body.to_s
+    end
+
+    test "should not provide the URL of the build in the notification email if dashboard url is not set" do
+      Configuration.stubs(:dashboard_url).returns(nil)
+      @notifier.emails = ['foo@happy.com']
+      @notifier.build_finished(failing_build)
+
+      mail = ActionMailer::Base.deliveries[0]
+      assert_no_match /http:\/\/www.my.com\/builds\/myproj\/5/, mail.body.to_s
     end
 
     test "should list the build info in the notification email if the dashboard url is not set" do
@@ -119,14 +116,15 @@ class EmailNotifierTest < ActiveSupport::TestCase
       @notifier.build_finished(failing_build)
 
       mail = ActionMailer::Base.deliveries[0]
-      assert_match /Note: if you set Configuration\.dashboard_url in site_config\.rb/, mail.body.to_s
+      assert_match /tests passed/, mail.body.to_s
     end
   end
 
   private
-  
+
   def failing_build
     @build.stubs(:failed?).returns(true)
+    @build.stubs(:status).returns("failed")
     @build.stubs(:output).returns(BUILD_LOG)
     @build
   end
